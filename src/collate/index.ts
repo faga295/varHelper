0d742d58f96982d4e98bdb715f8c2583f6ec321c
lzc <1064827638@qq.com>
2022-03-24 17:26:03 +0800

    first commit

---
+++ src/collate/index.ts
@@ -0,0 +1,12 @@
+import type { TextEditor } from 'vscode';
+import { BaiduFanyiAPI } from 'baidu-fanyi-api';
+export async function collate(editor:TextEditor){
+    const text = editor.document.getText(editor.selection);
+    const api = new BaiduFanyiAPI();
+    await api.init();
+    const { trans_result } = await api.translate(text,'zh','en');
+    console.log(trans_result);
+    editor.edit((editBuilder)=>{
+        editBuilder.replace(editor.selection,trans_result.data[0].dst);
+    });
+}
